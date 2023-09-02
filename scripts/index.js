4d641c3288fb18becd1aa4ee12a9c4d112290010
WRFMIXO <66543957+WRFMIXO@users.noreply.github.com>
2023-09-02 11:26:20 +0100

    Mise à jour du Css statique Résolution d'écran effective 1920 * 1080

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+function show() {
+    const dropdownButton = document.getElementById();
+    const hiddenItem = document.getElementById();
+    
+    if(dropdownButton == document.getElementById('dropdownButton') && hiddenItem == document.getElementById('hiddenItem')){
+        dropdownButton.addEventListener('click', () => {
+            hiddenItem.style.display = "block";
+        });
+    } else if(dropdownButton == document.get)
+}
